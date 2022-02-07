8a5ac29ae620726a08aa2748cf1f815b5ac21719
PS <89146019257@mail.ru>
2022-02-07 22:24:25 +0900

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int cost) {
+        int price = 10_000;
+        int amount = 20;
+        int miles = price / amount;
+
+        return miles;
+    }
+}
